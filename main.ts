434458e5cab93c44ad203da4f3107c1a2d318c51
Vincent Pham <vincent.pham013@gmail.com>
2021-04-08 18:15:30 +0700

    Init

--- main.ts
+++ main.ts
@@ -1,5 +1,39 @@
-function main() {
-  console.log('Hello Typescript!');
+import { AppSyncClient, CreateFunctionInput, CreateFunctionCommand } from '@aws-sdk/client-appsync-node';
+import { fromIni } from '@aws-sdk/credential-provider-ini';
+
+async function executor() {
+  try {
+    const appSync = new AppSyncClient({
+      credentials: fromIni(),
+    });
+
+    const params: CreateFunctionInput = {
+      apiId: 'pazltyadm5d2dbv2exr5d6oqam',
+      name: 'findOneEvent',
+      description: 'FindOneEvent function return a single event',
+      dataSourceName: 'AppSyncEventTable',
+      requestMappingTemplate: `{
+        "operation": "GetItem",
+        "key": {
+          "id": $util.dynamodb.toDynamoDBJson($ctx.args.id),
+        }
+      }`,
+      functionVersion: '2018-05-29',
+      responseMappingTemplate: `
+        #if($ctx.error)
+            $util.error($ctx.error.message, $ctx.error.type)
+        #end
+        $util.toJson($ctx.result)
+      `
+    }
+
+    const createFunctionsCommand = new CreateFunctionCommand(params);
+
+    const createdFunction = await appSync.send(createFunctionsCommand);
+    console.log("🚀 ~ file: main.ts ~ line 35 ~ executor ~ createdFunction", createdFunction)
+  } catch (error) {
+    console.log("🚀 ~ file: main.ts ~ line 18 ~ executor ~ error", error);
+  }
 }
 
-main();
+executor();
